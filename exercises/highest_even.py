428ffbf91763ebe09941c5ef9c70f8bc855c2a4b
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-01-29 21:43:37 -0300

    Add fix highest even

--- exercises/highest_even.py
+++ exercises/highest_even.py
@@ -1,15 +1,21 @@
 def highest_even(lista=[0]):
     max_num = 0
     for i in lista:
+        """
+            Becareful when using remove like this.
+            With remove, you change its size and cause it to skip elements
+        """
         if i % 2 != 0:
             lista.remove(i)
+    # lista = [x for x in lista if x % 2 != 0] <- use this.
+    print(lista)
     # or just return max(lista)
+
     for i in lista:
         if i > max_num:
             max_num = i
     return max_num
 
-
-print(highest_even([1,2,3,4,5,6,10,11,20,51,60]))
-print(highest_even())
-            
+lista = [1,2023,3,4444,5,6,10,1221,20,51,60111]
+lista = highest_even(lista)
+print(lista)

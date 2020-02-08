acaea1c43f3d0346137ab702d14c6cb577b4da82
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-02-08 10:32:48 -0300

    Add password checker

--- projects/passwordchecker/pass_check.py
+++ projects/passwordchecker/pass_check.py
@@ -1,29 +1,58 @@
 import requests
 
 import hashlib
+import sys
+
 # using SHA1 Hash
 # Hash is only one way function
 # using anonymous key, sending only the 5 characters
-"""
-the api checks if there is hashs with the same 5 first characters
-and send back all of those to check with our full hash.
-"""
+# The API will return the matches and how many times was pwned
+# The program will check if matches with the entere pwd and shows the result.
+
 
 def request_api_data(querychar):
+    """
+    the api checks if there is hashs with the same 5 first characters(in sha1)
+    and send back all matches. But will return without the 5 frist characters.
+    """
     if not(isinstance(querychar, str)):
         print('The password must be string!')
         return
     url = f'https://api.pwnedpasswords.com/range/{querychar}'
     r = requests.get(url)
-    if r.status != 200:
+    if r.status_code != 200:
         raise RuntimeError(f"Error fetching: {r.status_code}, check the api and try again")
+    return r
+
+
+def get_pass_leaks_count(hashes, hash_to_check):
+    hashs = (line.split(':') for line in hashes.text.splitlines())
+    # will return a generator with tuples and each tuple has the hash and number
+    for h, count in hashs:
+        if h == hash_to_check:
+            return count
+    return 0
+
 
 def pwned_api_check(password):
     # check password if it exists in API response
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
     #password.encode transform into b''
-    print(sha1password)
-    return sha1password
+    first5, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5)
+    # return read_res(response)
+    return get_pass_leaks_count(response, tail)
 
-pwned_api_check('123213')
+
+def main(args):
+    for pwd in args:
+        count = pwned_api_check(pwd)
+        if count:
+            print(f"{pwd} was found {count} times... you should change your password")
+        else:
+            print(f"{pwd} was not found! Nice!")
+    return 'Done!'
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
     

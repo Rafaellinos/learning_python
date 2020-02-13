9b5056704b3dd5576e2810f431d6d5477f9388a3
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-02-13 09:04:00 -0300

    Add new flask site

--- projects/website/flask_project/server.py
+++ projects/website/flask_project/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__) # name of main file __name__ == __main__
 """
@@ -8,6 +8,21 @@ export FLASK_APP=server.py
 export FLASK_ENV=development #debug mode
 flask run
 """
+@app.route('/home')
 @app.route('/')
-def hello_world():
+def home():
     return render_template('./index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    try:
+        return render_template(f"{page_name}.html")
+    except Exception as err:
+        return f"page not found {err}"
+
+@app.route('/submit_form', methods=['POST','GET'])
+def submit_form():
+    return 'form submitted hooray'
+
+if __name__ == '__main__':
+    app.run(debug=True)

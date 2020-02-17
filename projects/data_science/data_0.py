ce1e547e105e0f82f35522fd5ca84dec32f60e1e
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-02-17 08:37:14 -0300

    Add requirements for flask project, data science project and ignore data frame

---
+++ projects/data_science/data_0.py
@@ -0,0 +1,20 @@
+"""
+steps:
+
+1. import the data
+2. clean the data
+3. split data. Training set | test set
+4. create a model import
+5. check the output
+6. improve
+"""
+"""
+toos:
+numPy -> Helps to use list and arrays in multidimensional ways
+pandas -> allows to manipulate data from spreadsheets
+scikit-learn -> comes with algorithms pre build to use
+mathplotlib -> visualize data in nice way
+kaggle -> provide datasets
+jupyter nootebooks -> allows to step through the each data analise and visualize through grapics, thing that
+editors like vscode, pycharm, atom etc can't
+"""

f0e0b67900a80a6ef48904d6d9f9492e08757740
Rafael Lino <rafael.veloso.lino@hotmail.com>
2020-03-29 14:08:27 -0300

    Add get cases function and main

--- projects/coronavitus_update/coronaritus.py
+++ projects/coronavitus_update/coronaritus.py
@@ -9,11 +9,13 @@ pip3 install beautifulsoup4
 pip3 install plyer
 """
 
-res = requests.get('https://www.worldometers.info/coronavirus/').text
-soup = BeautifulSoup(res, 'html.parser')
-soup.encode('utf-8')
-cases = soup.find_all("div", {'class': 'maincounter-number'})
-cases = [case.get_text().strip() for case in cases]
+def get_cases() -> list:
+    res = requests.get('https://www.worldometers.info/coronavirus/').text
+    soup = BeautifulSoup(res, 'html.parser')
+    soup.encode('utf-8')
+    cases = soup.find_all("div", {'class': 'maincounter-number'})
+    cases = [case.get_text().strip() for case in cases]
+    return cases
 
 def notify_me(tittle, message):
     notification.notify(
@@ -21,6 +23,8 @@ def notify_me(tittle, message):
         message = message,
         timeout = 5)
 
-while True:
-    notify_me('Corona Alert', f'Total cases {cases[0]} Deaths {cases[1]} Recovered {cases[2]}')
-    time.sleep(600)
+if __name__ == '__main__':
+    while True:
+        cases = get_cases()
+        notify_me('Corona Alert', f'Total cases {cases[0]} Deaths {cases[1]} Recovered {cases[2]}')
+        time.sleep(600)

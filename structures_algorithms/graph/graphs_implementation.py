80b6851cc62dd8db93940c41bba931824295297d
Rafael Veloso <rafael.veloso.lino@hotmail.com>
2020-04-14 20:55:27 -0300

    Add graph implementation

--- structures_algorithms/graph/graphs_implementation.py
+++ structures_algorithms/graph/graphs_implementation.py
@@ -4,4 +4,22 @@ class Graph(object):
         self.adjacentlist = dict()
 
     def add_vertex(self, node):
-        pass
+        self.adjacentlist.update({
+            node: []
+        })
+        self.n_nodes += 1
+
+    def addEdge(self, node1, node2):
+        self.adjacentlist[node1].append(node2)
+        self.adjacentlist[node2].append(node1)
+        return self.adjacentlist
+
+    def print_connections(self):
+        for k,v in self.adjacentlist.items():
+            print(str(k) + "-->"+"".join(str(i) for i in v))
+
+graph = Graph()
+graph.add_vertex(1)
+graph.add_vertex(2)
+print(graph.addEdge(1,2))
+graph.print_connections()
